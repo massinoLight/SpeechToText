157ce485756db37168fe9b5cafb5e7b176840411
massino <nacerddine.amassin@gmail.com>
2020-09-08 02:51:38 +0200

    commit le speech est ajouté a la base de donnée Firestore
    to do
    recupérer la localisation et calcule de la distance

--- app/src/main/java/com/massino/speechtotext/MainActivity.java
+++ app/src/main/java/com/massino/speechtotext/MainActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONArray;
@@ -15,11 +17,22 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
+    private static final String COLLECTION_NAME = "speech";
+    FirebaseFirestore db;
 
     private TextView txvResult;
     JSONObject speech = new JSONObject();
@@ -32,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txvResult = (TextView) findViewById(R.id.txvResult);
+        // Access a Cloud Firestore instance from your Activity
+        db = FirebaseFirestore.getInstance();
+
     }
 
     public void getSpeechInput(View view) {
@@ -63,27 +79,10 @@ public class MainActivity extends AppCompatActivity {
                         speech.put("speech",result.get(0));
                         speech.put("DateTime", currentDateTimeString);
                         //speech.put("location", location);
-
-                    } catch (JSONException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-
-
-                    try {
                         Log.e(TAG, speech.getString("id"));
                         Log.e(TAG, speech.getString("speech"));
                         Log.e(TAG, speech.getString("DateTime"));
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    JSONArray jsonArray = new JSONArray();
-                    jsonArray.put(speech);
-
-                    try {
-                        speechObject.put("speeches", jsonArray);
+                        addSpeech(id,speech.getString("speech"),speech.getString("DateTime"));
 
                     } catch (JSONException e) {
                         // TODO Auto-generated catch block
@@ -96,4 +95,34 @@ public class MainActivity extends AppCompatActivity {
         }
         id++;
     }
+
+    public void addSpeech(int id,String leSpeech,String date) {
+        // [START add_ada_lovelace]
+        // Create a new user with a first and last name
+        Map<String, Object> speech = new HashMap<>();
+        speech.put("id", id);
+        speech.put("speech", leSpeech);
+        speech.put("DateTime", date);
+
+        // Add a new document with a generated ID
+        db.collection("speech")
+                .add(speech)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.e(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/massino/speechtotext/Speech.java
@@ -0,0 +1,17 @@
+package com.massino.speechtotext;
+
+public class Speech {
+
+    private int id;
+    private String speech;
+    private String date;
+
+    public Speech(int id,String speech,String date ){
+        this.id=id;
+        this.speech=speech;
+        this.date=date;
+
+    }
+
+
+}
